04fc7e44c5733e39be0b089e747f7bc9ee673c13
Edgar Leiva <99515843+Leiiiva@users.noreply.github.com>
2022-02-15 15:44:25 -0600

    Finished v1.0
    Completed homework

--- Tarea4/Tarea4/Form1.Designer.vb
+++ Tarea4/Tarea4/Form1.Designer.vb
@@ -22,10 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn_calcular = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_num = New System.Windows.Forms.TextBox()
+        Me.lbl_fact = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_calcular
+        '
+        Me.btn_calcular.Font = New System.Drawing.Font("Acumin Pro", 9.749999!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.btn_calcular.Location = New System.Drawing.Point(180, 67)
+        Me.btn_calcular.Name = "btn_calcular"
+        Me.btn_calcular.Size = New System.Drawing.Size(75, 32)
+        Me.btn_calcular.TabIndex = 0
+        Me.btn_calcular.Text = "Calcular"
+        Me.btn_calcular.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(53, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Calcular factorial de:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(53, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 21)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "El Factorial es :"
+        '
+        'txt_num
+        '
+        Me.txt_num.Font = New System.Drawing.Font("Acumin Pro", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txt_num.Location = New System.Drawing.Point(53, 72)
+        Me.txt_num.Name = "txt_num"
+        Me.txt_num.Size = New System.Drawing.Size(100, 23)
+        Me.txt_num.TabIndex = 3
+        Me.txt_num.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lbl_fact
+        '
+        Me.lbl_fact.AutoSize = True
+        Me.lbl_fact.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_fact.ForeColor = System.Drawing.Color.Navy
+        Me.lbl_fact.Location = New System.Drawing.Point(190, 117)
+        Me.lbl_fact.Name = "lbl_fact"
+        Me.lbl_fact.Size = New System.Drawing.Size(33, 25)
+        Me.lbl_fact.TabIndex = 4
+        Me.lbl_fact.Text = "---"
+        Me.lbl_fact.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(314, 176)
+        Me.Controls.Add(Me.lbl_fact)
+        Me.Controls.Add(Me.txt_num)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_calcular)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "201314707 - Edgar Leiva"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btn_calcular As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_num As TextBox
+    Friend WithEvents lbl_fact As Label
 End Class
--- Tarea4/Tarea4/Form1.vb
+++ Tarea4/Tarea4/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
-
+    Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
+        Dim num, x, i As Integer
+        x = 1
+        num = txt_num.Text
+        For i = 1 To num
+            x = x * i
+        Next
+        lbl_fact.Text = x.ToString()
+    End Sub
 End Class
